b4d3de2ccf9d2f5415968ff3020252a64942e0bd
sai <sai@digital-lync.com>
2022-04-08 10:01:25 +0530

    added AUTH

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 var UserModel = require('../models/user.model');
+var jwt = require('jsonwebtoken');
 exports.register = (req,res) => {
     var body = req.body;
     
@@ -16,4 +17,38 @@ exports.register = (req,res) => {
              }
          }
      })
+}
+
+exports.login = (req, res) =>{
+
+    var body = req.body;
+    var emailId = body.emailId;
+    var password = body.password;
+    console.log(emailId);
+
+    UserModel.find({emailId:emailId},(err,result) => {
+         if(err){
+             res.send(err.message)
+         }
+         if(result.length > 0){
+             var user = result[0];
+              if(user.password == password){
+                  
+                var _id= user.id;
+                  var token = jwt.sign( {payload:_id},'dl');
+                   res.send({token:token, isLoggedIn: true});
+              }
+              else
+              {
+                  res.send("password incorrect");
+              }
+         }
+         else
+         {
+             res.send("this email is not registered with us!!");
+         }
+          
+    })
+
+
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,5 +4,6 @@ var UserController = require('../controllers/user.controller');
 var router = express.Router();
 
 router.post('/register',UserController.register);
+router.post('/login', UserController.login);
 
 module.exports = router;
